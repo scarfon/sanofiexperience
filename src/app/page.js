7730893b6c3fb9f4c1ae509477635e8445325a02
Andres <95327624+scarfon@users.noreply.github.com>
2023-10-24 21:01:48 -0300

    delete console.log

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
 	useEffect(() => {
 		const dbRefImgUrl = ref(rtdb, "nota/imgUrl");
 		onValue(dbRefImgUrl, (snapshot) => {
-			console.log(snapshot.val());
 			setImg(snapshot.val());
 		});
 	}, []);
